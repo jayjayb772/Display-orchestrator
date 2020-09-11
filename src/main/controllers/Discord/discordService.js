c9f2c2fda343b3b86a75a533905e6e2dfca18b2e
Jacob <jayjayb100@msn.com>
2020-09-11 18:52:51 -0500

    again

--- src/main/controllers/Discord/discordService.js
+++ src/main/controllers/Discord/discordService.js
@@ -1,12 +1,20 @@
 const {debuglog} = require("../../util/debugCommands");
 const request = require('request')
+
+function options(body) {
+    return {
+        "headers": {
+            "content-type": "application/json"
+        },
+        "body": body
+    }
+}
 function sendDiscordMessage(reqBody, resA){
-    debuglog(reqBody);
     return new Promise(((resolve, reject) => {
         let msg = {
                         "content": `${reqBody.message}`
                   }
-        request.post(`${process.env.discordWebhookUrl}`, msg, (err,res)=>{
+        request.post(`${process.env.discordWebhookUrl}`, options(reqBody), (err,res)=>{
             if(err){
                 reject(err);
             }
