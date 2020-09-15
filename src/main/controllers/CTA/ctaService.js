7c057e9e6376e3d29627d8f0992516a8100e6290
Jacob <jayjayb100@msn.com>
2020-09-15 17:49:57 -0500

    ap json

--- src/main/controllers/CTA/ctaService.js
+++ src/main/controllers/CTA/ctaService.js
@@ -1,15 +1,14 @@
 const request = require('request');
 
 async function getTrainByStationAndColor(stationName, color){
-    return new Promise(async function (resolve, reject) {
         let url = `${process.env.CTAPI_URL}/orchestrator/train-times?name=${stationName}&color=${color}`;
         request.get(url, (err,res)=>{
             if(err){
-                reject(err);
+                throw err;
             }
-            resolve(res.body);
+            return(res.body);
         })
-    });
+
 }
 
 module.exports = {getTrainByStationAndColor}
