ba824ed92fb73c6547f412e183f9219f58195600
Jacob <jayjayb100@msn.com>
2020-09-23 00:01:16 -0500

    commit messages

--- src/main/controllers/commit-messages/commitService.js
+++ src/main/controllers/commit-messages/commitService.js
@@ -1,24 +1,26 @@
 const request = require('request')
-function parseMessage(body){
+
+function parseMessage(body) {
     console.log(JSON.stringify(body))
-    let message = {
-        embed: {
-            title: `${body.commits[0].message}`,
-            description: `Modified Repo: ${body.repository.name}`,
-            url: `${body.html_url}`,
-            color: "#FF0000"
+    let message = {}
+    let options = {
+        headers: {
+            "Content-type": "application/json"
+        },
+        content: {
+            embed: {
+                title: `${body.commits[0].message}`,
+                description: `Modified Repo: ${body.repository.name}`,
+                url: `${body.html_url}`,
+                color: "#FF0000"
+            }
         }
     }
-    let options = {
-        headers:{
-            "Content-type":"application/json"
-        },
-        content:JSON.stringify(message)
-    }
 
-request.post(`${process.env.DISCORD_COMMIT_URL}`, options, (err, res)=>{
-    console.log(res)
-} )
+    request.post(`${process.env.DISCORD_COMMIT_URL}`, options, (err, res) => {
+        console.log(res)
+    })
 
 }
+
 module.exports = {parseMessage}
