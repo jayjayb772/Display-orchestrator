aecc408a649244368ccf43166c473db50c9bb3e4
Jacob <jayjayb100@msn.com>
2020-09-22 23:35:17 -0500

    remove console.logs

--- sock.js
+++ sock.js
@@ -4,19 +4,19 @@ const clients=[]
 const echo = sockjs.createServer({ prefix:'/websocket' });
 echo.on('connection', function(conn) {
     clients.push(conn)
-    console.log("New connection")
-    console.log(conn)
+    //console.log("New connection")
+    //console.log(conn)
     conn.on('data', function(message) {
 
         clients.forEach(c=>{
             c.write(message)
         })
         console.log('message recieved')
-        console.log(message)
+        //console.log(message)
 
     });
     conn.on('close', function(con) {
-        console.log("Close Connection")
+        //console.log("Close Connection")
         clients.filter(c=> c !== con)
     });
 });
