4886ceff07985e7485969616cde2bbf25e7816d9
Jacob <jayjayb100@msn.com>
2020-09-22 23:51:18 -0500

    commit messages

--- src/main/controllers/commit-messages/commitService.js
+++ src/main/controllers/commit-messages/commitService.js
@@ -1,5 +1,21 @@
+const request = require('request')
 function parseMessage(body){
     console.log(JSON.stringify(body))
+    let message = {
+        embed: {
+            title: `${body.commits[0].message}`,
+            description: `Modified Repo: ${body.repository.name}`,
+            url: `${body.html_url}`,
+            color: "#FF0000"
+        }
+    }
+    let options = {
+        body:JSON.stringify(message)
+    }
+
+request.post(`${process.env.DISCORD_COMMIT_URL}`, options, (err, res)=>{
+    console.log(res)
+} )
 
 }
 module.exports = {parseMessage}
--- src/main/controllers/zzz-template/controllerService.js
+++ src/main/controllers/zzz-template/controllerService.js
@@ -1,5 +1,9 @@
 function serviceCall(){
     return "Hello world!";
+
+
+
+
 }
 
 module.exports = {serviceCall}
